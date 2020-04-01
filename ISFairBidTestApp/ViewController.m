b5a564fd9043a9b0d974acd6106ccaffc0bbff6c
Maciek Czapnik <maciej.czapnik@fyber.com>
2020-04-01 16:01:50 +0200

    Simple flow

--- ISFairBidTestApp/ViewController.m
+++ ISFairBidTestApp/ViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "ViewController.h"
+#import <IronSource/IronSource.h>
 
-@interface ViewController ()
+#warning(please fill statics: mediationType, appKey, firstInterstitialInstancId and secondInterstitialInstanceId)
+static NSString *const mediationType = @"";
+static NSString *const appKey = @"";
+
+static NSString *const firstInterstitialInstanceId = @"";
+static NSString *const secondInterstitialInstanceId = @"";
+
+@interface ViewController () <ISDemandOnlyInterstitialDelegate>
+
+@property (weak, nonatomic) IBOutlet UIView *firstInstanceAvailabilityView;
+@property (weak, nonatomic) IBOutlet UIView *secondInstanceAvailabilityView;
 
 @end
 
@@ -16,8 +27,67 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.firstInstanceAvailabilityView.backgroundColor = [UIColor grayColor];
+    self.secondInstanceAvailabilityView.backgroundColor = [UIColor grayColor];
+    
+    //setting ISDemanOnly delegates
+    [IronSource setISDemandOnlyInterstitialDelegate:self];
+    
+    //setting mediation type
+    [IronSource setMediationType:mediationType];
+    
+    //initializing
+    [IronSource initISDemandOnly:appKey adUnits:@[IS_INTERSTITIAL]];
+
 }
 
+- (IBAction)loadFirstInstance:(id)sender {
+    [IronSource loadISDemandOnlyInterstitial:firstInterstitialInstanceId];
+}
+
+- (IBAction)loadSecondInstance:(id)sender {
+    [IronSource loadISDemandOnlyInterstitial:secondInterstitialInstanceId];
+}
+
+- (IBAction)showFirstInstance:(id)sender {
+    if ([IronSource hasISDemandOnlyInterstitial:firstInterstitialInstanceId]) {
+        [IronSource showISDemandOnlyInterstitial:self instanceId:firstInterstitialInstanceId];
+    }
+}
+
+- (IBAction)showSecondInstance:(id)sender {
+    if ([IronSource hasISDemandOnlyInterstitial:secondInterstitialInstanceId]) {
+        [IronSource showISDemandOnlyInterstitial:self instanceId:secondInterstitialInstanceId];
+    }
+}
+
+#pragma mark ISDemandOnlyInterstitialDelegate
+
+- (void)interstitialDidLoad:(NSString *)instanceId {
+    [self reportAvailability];
+}
+
+- (void)interstitialDidFailToLoadWithError:(NSError *)error instanceId:(NSString *)instanceId {
+}
+
+- (void)interstitialDidOpen:(NSString *)instanceId {
+}
+
+- (void)interstitialDidClose:(NSString *)instanceId {
+    [self reportAvailability];
+}
+
+- (void)interstitialDidFailToShowWithError:(NSError *)error instanceId:(NSString *)instanceId {
+}
+
+- (void)didClickInterstitial:(NSString *)instanceId {
+}
+
+#pragma mark ad instanceid availability check
+
+- (void)reportAvailability {
+    self.firstInstanceAvailabilityView.backgroundColor = [IronSource hasISDemandOnlyInterstitial:firstInterstitialInstanceId] ? [UIColor greenColor] : [UIColor redColor];
+    self.secondInstanceAvailabilityView.backgroundColor = [IronSource hasISDemandOnlyInterstitial:secondInterstitialInstanceId] ? [UIColor greenColor] : [UIColor redColor];
+}
 
 @end
